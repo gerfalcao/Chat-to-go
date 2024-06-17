f854ea6e69a7b0570218b92a064a1b59b2b3868b
Germanno Falcão <germannoiknow@gmail.com>
2024-06-17 20:15:09 -0300

    Feat: Pool connections!

--- backend/main.go
+++ backend/main.go
@@ -2,27 +2,37 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gerfalcao/Chat-to-go.git/pkg/websocket"
 )
 
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := websocket.Upgrade(w, r)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Websocket Endpoint Hit")
+	
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(w, "%+v\n", err)
 	}
 
-	websocket.Reader(ws)
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+
 }
 
 func setupRoutes(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws",func(w http.ResponseWriter, r *http.Request){
+		serveWs(pool, w, r)
 	})
-	http.HandleFunc("/ws",serveWs)
 }
 
 func main(){
